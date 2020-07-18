84ba4e7533e98a9c4d43b845684012997619bf0b
harish <harishonkar.heballi@gmail.com>
2020-07-18 18:28:56 +0530

    70 to 80% of models are created

--- community_matrimony/matrimony/admin.py
+++ community_matrimony/matrimony/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Appuser,groom_bride_details,Kula_osa,States,District
 # Register your models here.
+admin.site.register(Appuser)
+admin.site.register(groom_bride_details)
+admin.site.register(Kula_osa)
+admin.site.register(States)
+admin.site.register(District)
--- community_matrimony/matrimony/models.py
+++ community_matrimony/matrimony/models.py
@@ -3,28 +3,49 @@ from django.db import models
 # Create your models here.
 
 
-# table to store state names
-class states(models.Model):
+
+class States(models.Model):
+    # table to store state names
     state_name = models.CharField(max_length= 60)
 
+    def __str__(self):
+        return self.state_name
 
-#table to store kula/osa names
-class kula_osa(models.Model):
+class District(models.Model):
+    district_name= models.CharField(max_length=50)
+    district_state=models.ForeignKey('States', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.district_name
+
+
+class Kula_osa(models.Model):
+
+    #table to store kula/osa names
     kula_name = models.CharField(max_length= 60)
+    kula_in_states = models.ForeignKey('States', on_delete=models.CASCADE)
 
-# user registration details.
+    def __str__(self):
+        return self.kula_name
+
+
 class Appuser(models.Model):
 
+    # user registration details.
     user_type= models.CharField(max_length= 30)
     first_name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
-    kula_osa=models.CharField(max_length=40)
-    mobile_no=models.CharField(max_length=10)
     occupation=models.CharField(max_length=30)
-    address=models.CharField(max_length=100)
+    mobile_no=models.CharField(max_length=10)
+    state=models.ForeignKey('States', on_delete=models.CASCADE)
+    district = models.ForeignKey('District', on_delete=models.CASCADE)
     city=models.CharField(max_length=40)
-    state=models.CharField(max_length=40)
+    kula_osa=models.ForeignKey('Kula_osa', on_delete=models.CASCADE) 
+    address=models.CharField(max_length=100) 
     created_date_time = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.mobile_no
 
 
 def user_directory_path(instance, filename): 
@@ -35,9 +56,11 @@ def user_directory_path(instance, filename):
 
 
 class groom_bride_details(models.Model):
+    
+    # table to store groom/bride details.
     name = models.CharField(max_length= 40)
     DOB = models.DateField()
-    height = models.IntegerField()
+    height = models.FloatField()
     occupation= models.CharField(max_length= 40)
     Father_name = models.CharField(max_length=40)
     mother_name = models.CharField( max_length=40)
@@ -45,8 +68,8 @@ class groom_bride_details(models.Model):
     created_date_time = models.DateTimeField(auto_now_add=True) 
     appuser = models.ForeignKey('Appuser', on_delete=models.CASCADE)
 
-
-
+    def __str__(self):
+        return self.name
 
 
 
